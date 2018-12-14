9fba0ee55f27a31fd7830d6ac6af7d4ff56b20cd
Hakim <hakimsembiring@outlook.co.id>
2018-12-14 19:37:37 +0700

    Login by username

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -3,7 +3,10 @@
 namespace App\Http\Controllers\Auth;
 
 use App\Http\Controllers\Controller;
+use Illuminate\Validation\Rule;
+use Illuminate\Http\Request;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
+use App\Models\User;
 
 class LoginController extends Controller
 {
@@ -36,4 +39,59 @@ class LoginController extends Controller
     {
         $this->middleware('guest')->except('logout');
     }
+
+    /**
+     * Determine if the request field is email or username.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return string
+     */
+    public function userField(Request $request)
+    {
+        return filter_var($request->get($this->username()), FILTER_VALIDATE_EMAIL) ? $this->username() : 'username';
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        $field = $this->userField($request);
+
+        return [
+            $field => $request->get($this->username()),
+            'password' => $request->password,
+        ];
+    }
+
+    /**
+     * Validate the user login request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return void
+     */
+    protected function validateLogin(Request $request)
+    {
+        $field = $this->userField($request);
+
+        $this->validate($request, [
+            $this->username() => "required|exists:users,{$field}",
+            'password' => 'required',
+        ], $this->validationError());
+    }
+
+    /**
+     * Response Validation Error
+     * @return array
+     */
+    public function validationError()
+    {
+        return [
+            $this->username() . '.exists' => 'This credential is invalid or you need to activate your account'
+        ];
+    }
+
 }
